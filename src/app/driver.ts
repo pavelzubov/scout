c47158743d1d741c6fdb7b3453ae54d3d71edb10
pasha224@yandex.ru <pasha1990>
2018-06-25 16:01:52 +0300

    Add interface driver

---
+++ src/app/driver.ts
@@ -0,0 +1,21 @@
+export interface Driver {
+  id: Wrapper;
+  name:  Wrapper;
+  avatar:  Wrapper;
+  phone:  Wrapper;
+  post:  Wrapper;
+  department:  Wrapper;
+  generalContractor:  Wrapper;
+  company: Wrapper;
+  lineManager:  Wrapper;
+  driverLicenseNumber:  Wrapper;
+  driverLicenseDate:  Wrapper;
+  experience:  Wrapper;
+  driverLicenseCategory: Wrapper;
+  driverTractLicenseCategory: Wrapper;
+}
+export interface Wrapper {
+  value: string;
+  doWrite: boolean;
+  doRead: boolean;
+}
