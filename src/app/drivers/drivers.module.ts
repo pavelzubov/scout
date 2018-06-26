8c7e99c7112158bcf735f9db5310e2338bbca2d9
pasha224@yandex.ru <pasha1990>
2018-06-26 08:23:54 +0300

    Add module and components

---
+++ src/app/drivers/drivers.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DriversRoutingModule } from './drivers-routing.module';
+import { DriversComponent } from './drivers/drivers.component';
+import { DriverComponent } from './driver/driver.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    DriversRoutingModule
+  ],
+  declarations: [DriversComponent, DriverComponent]
+})
+export class DriversModule { }
