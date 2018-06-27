5bb002b26920c157fb47150b4a079b986f6cbbd1
pasha224@yandex.ru <pasha1990>
2018-06-27 16:24:13 +0300

    Edit base and service

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -1,44 +1,80 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {from, Observable, throwError} from 'rxjs';
+import {from, Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {DriverList, Driver} from './driver';
-import {catchError} from 'rxjs/internal/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
+  public list: DriverList;
 
   constructor(private http: HttpClient) {
+    this.getListFromJson().subscribe(res => {
+      this.list = res;
+    });
+  }
+
+  getListFromJson(): Observable<any> {
+    return this.http.get('/assets/data.json');
   }
 
   getList(): Observable<DriverList> {
-    return this.http.get('/assets/data.json').pipe(
+    return from(new Promise((resolve, reject) => {
+      if (this.list) {
+        resolve(this.list);
+      } else {
+        this.getListFromJson().subscribe(res => {
+          this.list = res;
+          resolve(this.list);
+        }, err => {
+          reject(err);
+        });
+      }
+    })).pipe(
       map(res => <DriverList>res)
     );
   }
 
   getItem(id: number): Observable<Driver> {
-    return this.http.get('/assets/data.json').pipe(
-      map(res => <DriverList>res),
-      map(res => {
+    return from(new Promise((resolve, reject) => {
+      if (this.list) {
         // Немного имитации серверной логики
         // Если в базе нет записи с таким id, то возвращаем ошибку
         let driver;
-        if (driver = res.DriverList.find(item => item.id.value === id)) {
-          return <Driver>driver;
+        if (driver = this.list.DriverList.find(item => item.id.value === id)) {
+          resolve(<Driver>driver);
         } else {
-          throw new Error('Invalid id');
+          reject('Invalid id');
         }
-      })
-    );
+      } else {
+        this.getList().subscribe(res => {
+          this.getItem(id).subscribe(resl => {
+            resolve(<Driver>resl);
+          });
+        });
+      }
+    }));
   }
 
-  updateItem(item: Driver): Observable<any> {
-    return from(['true']).pipe(
-      map(res => 'Удачно')
-    );
+  updateItem(driver: Driver): Observable<any> {
+    return from(new Promise((resolve, reject) => {
+      // Еще немного имитации серверной логики
+      // Проходим по полям. Если поле изменено
+      // то проверяем, можно его изменять
+      const changingDriverId = this.list.DriverList.findIndex(item => item.id.value === driver.id.value);
+      for (const field in this.list[changingDriverId]) {
+        if (!this.list[changingDriverId].hasOwnProperty(field)) continue;
+        if (this.list[changingDriverId][field].value !== driver[field].value) {
+          if (!this.list[changingDriverId][field].canWrite) {
+            reject('This field can`t change');
+          }
+        }
+      }
+      this.list[changingDriverId] = driver;
+      resolve('Successful');
+    }));
   }
 
 }
